f86460011996e7c014b8f1bd9d0dda6ee2d92a5f
Cristian <cbtrigueros06@gmail.com>
2022-07-28 20:42:45 -0500

    2.1

--- public/server.js
+++ public/server.js
@@ -3,7 +3,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.get(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + '/public'));
 
 app.listen(port, () => {
   console.log(`Server running on localhost:${port}`);
