593394c7d9b064d568ef9d5c7e7f0cfc144efc5f
Cristian <cbtrigueros06@gmail.com>
2022-07-28 20:35:58 -0500

    heroku.2

--- public/server.js
+++ public/server.js
@@ -3,9 +3,7 @@ const app = express();
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
-});
+app.get(express.static(__dirname + '/public'));
 
 app.listen(port, () => {
   console.log(`Server running on localhost:${port}`);
