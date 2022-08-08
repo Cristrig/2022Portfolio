40721a3b320fc94868a54eb0ae4edb1c6375766d
Cristian <cbtrigueros06@gmail.com>
2022-08-08 12:58:13 -0500

    1

--- public/server.js
+++ public/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
+const path = require('node:path');
 
 const PORT = process.env.PORT || 3000;
 
-const path = require('path');
-app.use('/static', express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.listen(PORT, () => {
   console.log(`Server running on localhost:${PORT}`);
