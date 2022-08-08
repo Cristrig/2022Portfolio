dedb30d60d1d02d636615a48fdfa16cff0dee6ed
Cristian <cbtrigueros06@gmail.com>
2022-08-07 21:30:40 -0500

    procfile

--- public/server.js
+++ public/server.js
@@ -1,10 +1,10 @@
 const express = require('express');
 const app = express();
 
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public'));
 
 app.listen(port, () => {
-  console.log(`Server running on localhost:${port}`);
+  console.log(`Server running on localhost:${PORT}`);
 });
